36ca01a8520712255d302a7eeaa0cb9dfddcb5dd
mariKaren <karenbmari@gmail.com>
2026-02-22 20:10:20 +0100

    Setup TailwindCSS, React Router and Remix Icons

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#139ce0",
+        secondary: "#17a1cf",
+      },
+    },
+  },
+  plugins: [],
+}
+
